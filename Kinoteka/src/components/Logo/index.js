2a112a1cd1261e6afe6fc186cad7abd06f41631a
NediljkoIvisic <nediljko.ivisic99@gmail.com>
2021-01-03 19:38:49 +0100

    Creating logo component

---
+++ Kinoteka/src/components/Logo/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './style.module.css'
+
+const Logo = () => (
+    <section className={styles.logo}>
+        <span className={styles.logoInside}/>
+    </section>
+)
+
+export default Logo
--- Kinoteka/src/components/Navigation/index.js
+++ Kinoteka/src/components/Navigation/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 import styles from './style.module.css'
 import NavigationBar from '../NavigationBar'
+import Logo from '../Logo'
 
 const Navigation = ({activeTab}) => (
   <section className={styles.navigation}>
     <NavigationBar activeTab={activeTab} />
   </section>
 )
-  
+
 export default Navigation
