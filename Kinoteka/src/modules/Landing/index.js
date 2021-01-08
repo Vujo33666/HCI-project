f2c535fd57827b58eb3b1f30db4fe01d9053b679
Marko Vujnovic <markovujnovic3@gmail.com>
2021-01-08 14:05:48 +0100

    landing page js

--- Kinoteka/src/modules/Landing/index.js
+++ Kinoteka/src/modules/Landing/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
- 
-import Background from '../../components/BackgroundImage'
+ import Background from '../../components/BackgroundImage'
 import NavigationBar from '../../components/navigationBar'
 import styles from './style.module.css'
  
+
 const Landing = ({activeIntro}) => (
     <section className={styles.landing}>
         <Background activeIntro={activeIntro}/>
