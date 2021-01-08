599a9bfe05fdb49b669bcfe9a731f4b61bb3c7c3
NediljkoIvisic <nediljko.ivisic99@gmail.com>
2021-01-08 18:28:44 +0100

    more minor changes

--- Kinoteka/src/modules/Landing/index.js
+++ Kinoteka/src/modules/Landing/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
- 
+
 import Background from '../../components/BackgroundImage'
 import styles from './style.module.css'
- 
+
 const Landing = ({activeIntro}) => (
     <section className={styles.landing}>
         <Background activeIntro={activeIntro}/>
