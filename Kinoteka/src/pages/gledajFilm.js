d6973d5a12b69b19fbdd19ec5e2918f7cb2d8851
NediljkoIvisic <nediljko.ivisic99@gmail.com>
2021-01-10 13:34:23 +0100

    Adding playMovieImage and movieCard pictures

---
+++ Kinoteka/src/pages/gledajFilm.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import HeaderFooterLayout from "../layouts/HeaderFooterLayout"
+import PlayMoviePicture from "../components/PlayMovieBackground"
+ 
+const KategorijePage = () => (
+    <HeaderFooterLayout activeTab="">
+        <PlayMoviePicture />
+    </HeaderFooterLayout>
+)
+ 
+export default KategorijePage
