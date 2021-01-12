f15ede791a0b7010634c83fde9d11bf90e6fd675
NediljkoIvisic <nediljko.ivisic99@gmail.com>
2021-01-12 15:11:28 +0100

    adding top quote to forum pages

--- Kinoteka/src/pages/forumTopic.js
+++ Kinoteka/src/pages/forumTopic.js
@@ -2,9 +2,14 @@ import React from "react"
 
 import HeaderFooterLayout from "../layouts/HeaderFooterLayout"
 import TopicPool from "../modules/TopicPool"
+import Quote from "../components/Quote"
  
 const ForumTopicPage = () => (
     <HeaderFooterLayout activeTab="Forum">
+        <Quote 
+            quoteText="A good film is when the price of the dinner, the theatre admission and the baby sitter were worth it."
+            quoteBy="- Alfred Hitchcock"
+        />
         <TopicPool
             separatorText="pronađite film"
             quoteText={"They laught at me because I'm different.\nI laught at them because they're all the same."}
