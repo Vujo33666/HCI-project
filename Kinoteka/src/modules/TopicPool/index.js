8c3c7bdb5dc149a8f46b5ba7b61293b521a484bd
NediljkoIvisic <nediljko.ivisic99@gmail.com>
2021-01-12 11:44:50 +0100

    Adding topicPool

---
+++ Kinoteka/src/modules/TopicPool/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import styles from './style.module.css'
+import DiscussionTab from '../../components/DiscussionTab'
+import ContentSeparator from '../../components/ContentSeparator'
+import Quote from '../../components/Quote'
+import Button from '../../components/Button'
+
+const TopicPool = ({topicPoolWhere, separatorText, quoteText, quoteBy}) => {
+    <div className={styles.topic} >
+        <div className={styles.topicSeparator} >
+            <ContentSeparator separatorText={separatorText}/>
+        </div>
+        <div className={styles.topicPool} >
+            <Quote 
+                active="playMovie"
+                quoteText={quoteText}
+                quoteBy={quoteText}
+            />
+            <DiscussionTab topicText="rasprava 1" />
+            <DiscussionTab topicText="rasprava 2" />
+            <DiscussionTab topicText="rasprava 3" />
+            <Button activeButton="readMore" />
+        </div>
+    </div>
+}
+
+export default TopicPool
