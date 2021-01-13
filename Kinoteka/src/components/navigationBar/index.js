8b324d6861530160208afebd1fde5ad499e9184d
NediljkoIvisic <nediljko.ivisic99@gmail.com>
2021-01-13 19:19:59 +0100

    Adding some links

--- Kinoteka/src/components/navigationBar/index.js
+++ Kinoteka/src/components/navigationBar/index.js
@@ -1,18 +1,19 @@
 import React from 'react'
 
+import { Link } from 'gatsby'
 import styles from './style.module.css'
 import {navs as navigationTabs} from '../../constants/const'
 
 const NavigationBar = ({activeTab, navigationBarWhere}) => (
     <nav className={navigationBarWhere === "navigation" ? styles.navigationBarNav :  styles.navigationBar}>  
         {
-            navigationTabs.map(
-                tab =>  <li key={tab}
-                            className={tab === activeTab ? styles.active : styles.navLiTab}
-                        >
-                            {tab}
-                        </li>
-            )
+            navigationTabs.map(({tab,to}) => (
+                <Link to={to}>
+                    <li key={tab} className={tab === activeTab ? styles.active : styles.navLiTab}>
+                        {tab}
+                    </li>
+                </Link>
+            ))
         }
     </nav>
 )
